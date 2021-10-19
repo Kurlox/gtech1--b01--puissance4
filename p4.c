7228ca1c668e04fc339179721e8bd591d1807595
leannf <lfarant@GCFRLY090.GAMINGCAMPUS.local>
2021-10-19 17:16:38 +0200

    Affichage tableau

---
+++ p4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define NBL 6
+#define NBC 7
+char tab[NBL][NBC];
+char tokens[] = "ox";
+
+int init(void) {
+  for(int l=0;l<NBL;l++) {
+    for(int c=0;c<NBC;c++) {
+      tab[l][c] = '.';
+    }
+  }
+}
+int print(void) {
+  printf("-------------\n");
+  for(int l=0;l<NBL;l++) {
+    for(int c=0;c<NBC;c++) {
+      printf("%c ", tab[l][c]);
+    }
+    printf("\n");
+  }
+  printf("-------------\n");
+  printf("1 2 3 4 5 6 7\n");
+}
+
+int main(void) {
+  init();
+  printf("Welcome to Puissance4!\n");
+  print();
+
+  return 0;
+}
